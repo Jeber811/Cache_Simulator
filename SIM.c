bf8ad7f52abf5a0c0c0a617dfda82bcb708c5922
Jeber811 <jbwjake81@gmail.com>
2025-10-28 02:46:59 -0400

    Save SIM.c updates

--- SIM.c
+++ SIM.c
@@ -39,6 +39,25 @@ int main(int argc, char *argv[])
 
     NUM_SETS = cache_size / (BLOCK_SIZE * ASSOC);
 
+    // Allocate 2D arrays
+    tag_array = malloc(NUM_SETS * sizeof(long long int *));
+    lru_position = malloc(NUM_SETS * sizeof(long long int *));
+    dirty = malloc(NUM_SETS * sizeof(bool *));
+    for (int i = 0; i < NUM_SETS; i++)
+    {
+        tag_array[i] = malloc(ASSOC * sizeof(long long int));
+        lru_position[i] = malloc(ASSOC * sizeof(long long int));
+        dirty[i] = malloc(ASSOC * sizeof(bool));
+
+        // Initialize arrays
+        for (int j = 0; j < ASSOC; j++)
+        {
+            tag_array[i][j] = -1;   // empty slot
+            lru_position[i][j] = j; // initial LRU order
+            dirty[i][j] = false;    // clean block
+        }
+    }
+
     char op;
     long long int add;
 
@@ -91,11 +110,11 @@ void Simulate_access(char op, long long int add)
             // Choose policy (LRU or FIFO)
             if (REPLACEMENT == 0)
             { // LRU policy is chosen
-                Update_lru(add);
+                Update_lru(op, add);
             }
             else
             {
-                Update_fifo(add);
+                Update_fifo(op, add);
             }
             break; // Stop searching after a hit
         }
@@ -109,13 +128,89 @@ void Simulate_access(char op, long long int add)
     }
 }
 
-// Policy update functions
-void Update_lru(long long int add)
+void Update_lru(char op, long long int add)
 {
-    // TODO: Implement LRU update logic
+    int set = (add / BLOCK_SIZE) % NUM_SETS;
+    long long int tag = add / BLOCK_SIZE;
+
+    int hit_way = -1;
+    // Find which way was hit
+    for (int i = 0; i < ASSOC; i++)
+    {
+        if (tag_array[set][i] == tag)
+        {
+            hit_way = i;
+            break;
+        }
+    }
+
+    if (hit_way == -1)
+        return; // shouldn't happen on a hit
+
+    // If this is a write, mark the block as dirty
+    if (op == 'W')
+        dirty[set][hit_way] = true;
+
+    int old_lru = lru_position[set][hit_way];
+
+    // Increment LRU value of all blocks more recently used than this one
+    for (int i = 0; i < ASSOC; i++)
+    {
+        if (lru_position[set][i] < old_lru)
+        {
+            lru_position[set][i]++;
+        }
+    }
+
+    // Set this block as most recently used
+    lru_position[set][hit_way] = 0;
 }
 
-void Update_fifo(long long int add)
+void Update_fifo(char op, long long int add)
 {
-    // TODO: Implement FIFO update logic
+    int set = (add / BLOCK_SIZE) % NUM_SETS;
+    long long int tag = add / BLOCK_SIZE;
+
+    int hit_way = -1;
+    // Check if this block is already in cache (hit)
+    for (int i = 0; i < ASSOC; i++)
+    {
+        if (tag_array[set][i] == tag)
+        {
+            hit_way = i;
+            break;
+        }
+    }
+
+    if (hit_way != -1)
+    {
+        // FIFO: on a hit, do nothing
+        return;
+    }
+
+    // Miss: find the block to replace (FIFO = largest lru_position)
+    int victim_way = 0;
+    int max_pos = -1;
+    for (int i = 0; i < ASSOC; i++)
+    {
+        if (lru_position[set][i] > max_pos)
+        {
+            max_pos = lru_position[set][i];
+            victim_way = i;
+        }
+    }
+
+    // Insert the new block into victim way
+    tag_array[set][victim_way] = tag;
+    dirty[set][victim_way] = false; // or true if it's a write
+
+    // Increment all positions to maintain FIFO order
+    for (int i = 0; i < ASSOC; i++)
+    {
+        if (i != victim_way)
+            lru_position[set][i]++;
+    }
+
+    // Set victim to the "youngest" in FIFO
+    lru_position[set][victim_way] = 0;
 }
